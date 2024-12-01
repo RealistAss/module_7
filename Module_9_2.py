6fa4247c0b8f1a0d86023ec0c15fdf1e0b29a5ba
Илья <Ilyamolot@mail.ru>
2024-12-02 00:08:50 +0300

    print

---
+++ Module_9_2.py
@@ -0,0 +1,9 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+third_strings = first_strings + second_strings
+first_result = [len(i) for i in first_strings if len(i) > 5]
+second_result = [x + y for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {x: len(x) for x in third_strings if len(x) % 2}
+print(first_result)
+print(second_result)
+print(third_result)
