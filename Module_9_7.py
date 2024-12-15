e4b6c66011e471c3129e16a3ee8c55c37b3615d3
Илья <Ilyamolot@mail.ru>
2024-12-15 22:48:43 +0300

    print

---
+++ Module_9_7.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    def wrapper(*nums):
+        result = func(*nums)
+        if result < 2:
+            print('sostavnoe')
+            return
+        for div in range(2, result):
+            if result % div == 0:
+                print('sostavnoe')
+                break
+        else:
+            print('prostoe')
+
+
+    return wrapper
+@is_prime
+def sum_three(a, b, c):
+
+    return a + b + c
+
+result = sum_three(2, 3, 6)
